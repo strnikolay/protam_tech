527ab45e230c53db2ced691012066e40f4608b7e
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-14 23:55:17 +0300

    init project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# protam_tech
+
+
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './App.css';
+import Main  from './components/main/main';
+//import About from "./components/about/about";
+//import ErrorPage from "./components/404/404";
+
+
+
+
+
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main/>,
+      //errorElement: <ErrorPage />,
+    },
+    /*{
+      path: "/about",
+      element: <About />,
+    },*/
+  ]);
+
+
+
+  return (
+    <div className="App">
+    <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App; 
