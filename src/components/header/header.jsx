b42fec3185491fc9f4c798fe7168292007578c42
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-25 23:00:29 +0300

    25_11_23

---
+++ src/components/header/header.jsx
@@ -0,0 +1,25 @@
+import "./header.css"
+
+
+function Header () {
+
+return(  
+  <div className="header">
+
+      <div className="header-central-item">
+          <div className="menu">
+            <a href="/">Главная</a>
+            <a href="#etap">Этапы работы</a>
+            <div className="logo"></div>
+            <a href="#about">О нас</a>
+            <a href="#lead">Контакты</a>
+          </div>
+      </div>
+
+  </div>
+)
+}
+ 
+export default Header;
+
+ 
---
+++ src/components/header2/header.jsx
@@ -0,0 +1,28 @@
+import "./header.css"
+import S from "./subtract-WEs.png"
+import foneicon from "./basil-phone-solid-hNf.png"
+import searchicon from "./-CcF.png"
+import arrow from "./zondicons-arrow-up-1aX.png"
+
+function Header () {
+
+return(  
+  <div className="header">
+
+      <div className="header-central-item">
+          <div className="menu">
+            <a href="/">Главная</a>
+            <a href="#etap">Этапы работы</a>
+            <div className="logo"></div>
+            <a href="/about">О нас</a>
+            <a href="/qw"  >Контакты</a>
+          </div>
+      </div>
+
+  </div>
+)
+}
+ 
+export default Header;
+
+ 
