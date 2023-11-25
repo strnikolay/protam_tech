dfdd31457dcb3196e71b6d976ae10c9ae92d2ac1
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-25 23:01:02 +0300

    25_11_23

---
+++ src/components/main4/scr5_about/screen5.jsx
@@ -0,0 +1,68 @@
+import "./screen5.css"
+
+
+function MainEighthScreen () {
+
+return(
+    <div class="screen5" id="about">
+      {/*<div class="screen8-wrapper">*/}
+        <div class="title">
+            <p class="title-text">
+                <span class="black">Преимущества </span>
+                <span class="orange">Работы С нами</span>
+            </p>
+            {/*<img class="vector-144-Zpb" src={vector} alt=""/>*/}
+        </div>
+
+
+
+          <div class="content">
+            <div class="left-content">
+              {/*<div class="left-text">*/}
+                <div class="screen5-content-block left">
+                  <p class="screen5-content-block-title">Фокус на клиенте</p>
+                  <p class="screen5-content-block-text"> 
+                    <h2>
+                      Мы придерживаемся принципа ориентации на клиента и стараемся удовлетворить их потребности и ожидания. Наша команда специалистов всегда готова предоставить индивидуальную консультацию, помочь с выбором правильного погрузчика и обеспечить высококлассное обслуживание клиентов.
+                    </h2></p>
+                </div>
+                <div class="screen5-content-block left">
+                  <p class="screen5-content-block-title">Надежность</p>
+                  <p class="screen5-content-block-text">
+                    <h2>       
+                      Мы гарантируем надёжность и долговечность нашей техники. Все погрузчики, которые мы предлагаем, проходят строгий контроль качества, чтобы клиенты могли быть уверены в их надёжности и высокой производительности.
+                    </h2>
+</p>
+                </div>
+              {/*</div>*/}
+              
+            </div>
+            <div class="central-figure">
+                <div class="screen5-logo"></div>
+            </div>
+
+            <div class="right-content">
+              <div class="screen5-content-block right">
+                <p class="screen5-content-block-title">Удобство использования</p>
+                <p class="screen5-content-block-text">
+                  <h2>
+                  Мы предлагаем погрузчики, которые легки в использовании и обеспечивают максимальное удобство для операторов. Это повышает производительность и снижает риск возникновения ошибок или несчастных случаев на складе.
+                  </h2></p>
+              </div>
+              <div class="screen5-content-block right">
+                <p class="screen5-content-block-title">Инновации</p>
+                <p class="screen5-content-block-text">
+                  <h2>
+                  Мы ставим на инновации и всегда стремимся быть впереди отрасли. Предлагая клиентам современные технологические решения, мы помогаем им снизить издержки, увеличить эффективность и повысить безопасность на складе.
+                  </h2></p>
+              </div>
+            </div>
+          </div>
+        {/*</div>*/}
+
+    </div>
+
+
+)}
+
+export default MainEighthScreen;    
