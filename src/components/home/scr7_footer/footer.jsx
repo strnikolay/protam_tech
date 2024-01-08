71d0d01e68608967305a444e6c3f0d2c32ab61b9
Проходная <nikolayromanchuk88@gmail.com>
2024-01-08 23:57:31 +0300

    08_01_24

---
+++ src/components/home/scr7_footer/footer.jsx
@@ -0,0 +1,16 @@
+import "./footer.css"
+
+function MainFooter () {
+
+return(
+   
+  <footer className="footer">
+      <a href="protam.tech">&copy; 2023.   protam.tech   </a>
+  </footer>
+
+)}
+
+export default MainFooter;    
+
+
+
